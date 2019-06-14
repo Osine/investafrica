86dbb0be3530e3d741d78696bee5580212a7dbe9
Osine Ikhianosime <osikhianosime@gmail.com>
2019-06-14 18:21:40 +0100

    cities

--- index.php
+++ index.php
@@ -15,11 +15,7 @@
 
 </head>
 
-<div class="topnav">
-    <input type="text" placeholder="Opportunity awaits...">
-    <a class="active" href="#home">Home</a>
-    <a href="#about">About</a>
-    <a href="#contact">Contact</a>
+<?php include_once("/Applications/MAMP/htdocs/Doingbusinessinafrica/header.php"); ?>
 
 </div>
 
