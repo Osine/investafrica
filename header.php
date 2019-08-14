d39597fee9b9b685de1a52a5c8d1f00763fc0f23
Osine Ikhianosime <osikhianosime@gmail.com>
2019-08-14 14:28:49 +0100

    commented out the search bar because we don't have enough data yet

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 
 <div class="topnav">
     <img title="Lagos" src="investafrica.png" height="30px">
-    <input type="text" placeholder="Opportunity awaits...">
+    <!-- <input type="text" placeholder="Opportunity awaits..."> -->
     <a class="active" href="index.php">Home</a>
     <a href="#about">About</a>
     <a href="#contact">Contact</a>
