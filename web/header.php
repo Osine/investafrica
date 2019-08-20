4aca4668fdf60df233ebe828f39146bb39c68f6e
Osine Ikhianosime <osikhianosime@gmail.com>
2019-08-20 05:50:38 +0100

    other header issues

--- web/header.php
+++ web/header.php
@@ -2,7 +2,7 @@
 <div class="topnav">
     <img title="investafrica" src="images/investafrica.png" height="30px" href="index.php">
     <!-- <input type="text" placeholder="Opportunity awaits..."> -->
-    <a class="active" href="/Doingbusinessinafrica/index.php">Home</a>
+    <a class="active" href="index.php">Home</a>
     <a href="#about">About</a>
     <a href="#contact">Contact</a>
 
