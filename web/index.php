5173a46f2664ebc1ae2dcb78f207a5f0d011b38b
Osine Ikhianosime <osikhianosime@gmail.com>
2019-08-20 05:36:43 +0100

    testing heroku

--- web/index.php
+++ web/index.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'vendor/autoload.php';
-include_once('connection.php');
+// include_once('connection.php');
 // int $result = mixed;
-$query="select from TBL_NAME";
-$result = mysqli_query($query, 'mixed');
+// $query="select from TBL_NAME";
+// $result = mysqli_query($query, 'mixed');
 ?>
 <!DOCTYPE html>
 <html class="no-js">
