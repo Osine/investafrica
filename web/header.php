fef07d2b75c3be8352dbda4b5d8ac5f36e5232d6
Osine Ikhianosime <osikhianosime@gmail.com>
2019-08-20 12:48:19 +0100

    home image link

--- web/header.php
+++ web/header.php
@@ -1,9 +1,8 @@
-
 <div class="topnav">
-    <img title="investafrica" src="images/investafrica.png" height="30px" href="index.php">
+    <a href="index.php"> <img title="investafrica" src="images/investafrica.png" height="30px"></a>
     <!-- <input type="text" placeholder="Opportunity awaits..."> -->
-    <a class="active" href="index.php">Home</a>
     <a href="#about">About</a>
     <a href="#contact">Contact</a>
+    <a class="active" href="index.php">Home</a>
 
 </div>
