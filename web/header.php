12e1d2f545815a41bf93b09b3c3ca7e870f69e38
Osine Ikhianosime <osikhianosime@gmail.com>
2019-08-21 02:50:28 +0100

    finally mobile friendly. It's been a long night

--- web/header.php
+++ web/header.php
@@ -1,11 +1,12 @@
 <script src="javascript/myFunction.js"></script>
 <div class="topnav">
 <a href="index.php"> <img title="investafrica" src="images/investafrica.png" height="30px"></a>
+<a id= "name" href="index.php">Invest in Africa</a>
 <nav id="links">
     <!-- <input type="text" placeholder="Opportunity awaits..."> -->
-    <a href="#about">About</a>
+    <!-- <a href="#about">About</a>
     <a href="#contact">Contact</a>
-    <a class="active" href="index.php">Home</a>
+    <a class="active" href="index.php">Home</a> -->
 </nav>
 <a href="javascript:void(0);" class="icon" onclick="myFunction()">
 <i class="fa fa-bars"></i>
--- web/index.php
+++ web/index.php
@@ -17,7 +17,7 @@
     <link rel="javascript" href="javascript/expand_retract.js">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/main.css">
-    <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 <link rel="javascript" href="javascript/myFunction.js">
     <meta name="apple-mobile-web-app-capable " content="yes " />
     <meta name="apple-mobile-web-app-status-bar-style " content="black " />
