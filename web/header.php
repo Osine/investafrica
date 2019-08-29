2282f24acc95a8772ca3692ea14048eb3cd20cda
Osine Ikhianosime <osikhianosime@gmail.com>
2019-08-29 06:19:52 +0100

    design changes not done

--- web/header.php
+++ web/header.php
@@ -1,15 +1,23 @@
 <script src="javascript/myFunction.js"></script>
 <div class="topnav">
-<a href="index.php"> <img title="investafrica" src="images/investafrica.png" height="30px"></a>
-<a id= "name" href="index.php">Invest in Africa</a>
 <nav id="links">
+<a href="index.php"><img title="investafrica" src="images/investafrica.png" height="30px"></a>
+
     <!-- <input type="text" placeholder="Opportunity awaits..."> -->
+    <a class="active" href="index.php">Home</a>
     <!-- <a href="#about">About</a>
-    <a href="#contact">Contact</a>
-    <a class="active" href="index.php">Home</a> -->
+    <a class="login" href="#contact">Contact</a> -->
+
 </nav>
 <a href="javascript:void(0);" class="icon" onclick="myFunction()">
 <i class="fa fa-bars"></i>
 </a>
+</div>
+<div class ="hero-box">
+    <div class ="banner-text">
+        <p>Do Business across African Countries. Connect to African entrepreneurs and expand your business across Africa. </p>
+<div class="cta-box">
+    <button> INVEST NOW </button>
+</div>
 </div>
 
--- web/index.php
+++ web/index.php
@@ -4,147 +4,150 @@
 // $query="select from TBL_NAME";
 // $result = mysqli_query($query, 'mixed');
 ?>
-<!DOCTYPE html>
-<html class="no-js">
+    <!DOCTYPE html>
+    <html class="no-js">
 
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Invest in Africa</title>
-    <meta name="description" content="Find the nest countries to start your new business in africa.">
-    <script src="javascript/autoscroll.js"></script>
-    <script src="javascript/expand_retract.js"></script>
-    <link rel="javascript" href="javascript/autoscroll.js">
-    <link rel="javascript" href="javascript/expand_retract.js">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/main.css">
-    <!-- <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-<link rel="javascript" href="javascript/myFunction.js">
-    <meta name="apple-mobile-web-app-capable " content="yes " />
-    <meta name="apple-mobile-web-app-status-bar-style " content="black " />
-    <meta name="apple-mobile-web-app-title " content="Invest in Africa">
-    <meta name="apple-mobile-web-app-capable " content="yes ">
-    <meta name="apple-mobile-web-app-status-bar-style " content="white " />
-    <meta name=”viewport” content=”width=device-width, initial-scale=1″>
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Invest in Africa</title>
+        <meta name="description" content="Find the nest countries to start your new business in africa.">
+        <script src="javascript/autoscroll.js"></script>
+        <script src="javascript/expand_retract.js"></script>
+        <link rel="javascript" href="javascript/autoscroll.js">
+        <link rel="javascript" href="javascript/expand_retract.js">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/main.css">
+        <!-- <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+        <link rel="javascript" href="javascript/myFunction.js">
+        <meta name="apple-mobile-web-app-capable " content="yes " />
+        <meta name="apple-mobile-web-app-status-bar-style " content="black " />
+        <meta name="apple-mobile-web-app-title " content="Invest in Africa">
+        <meta name="apple-mobile-web-app-capable " content="yes ">
+        <meta name="apple-mobile-web-app-status-bar-style " content="white " />
+        <meta name=”viewport” content=”width=device-width, initial-scale=1″>
 
 
-</head>
+    </head>
 
-<?php include_once("header.php"); ?>
+    <?php include_once("header.php"); ?>
 
-<body>
+    <body>
 
-    <!-- <div class="filter">
+        <!-- <div class="filter">
         <button>Filter</button>
     </div> -->
-    <div class="hero">
-        <div class="cities">
-            <div class="gridview">
+        <div class="hero">
+            <div class="cities">
+                <div class="gridview">
 
-                <li class="Nigeria" style="background: url(images/lagos.jpg)">
-                    <div class="text">
-                        <h2 class="name" itemprop="name">
-                            <a onclick="popup" itemprop="url" href="Nigeria.php" title="Invest in Nigeria">Nigeria</a>
-                        </h2>
-                        <!-- <img title="Lagos" src="lagos.jpeg"> -->
-                    </div>
-                    <div class="action">
-                        <div class="main-score-rating">
-                            <div class="filling " style="width:100%">
-                                <div itemprop="aggregateRating " itemscope itemtype="http://schema.org/aggregateRating ">
-                                    <meta itemprop="name " content="Rating " />
-                                    <!-- <meta itemprop="worstRating " value="0 " /><span itemprop="ratingValue ">5</span> <span itemprop="bestRating ">5</span><br /><span class="reviewCount "><span itemprop="reviewCount ">209</span> reviews</span> -->
+                    <li class="Nigeria" style="background: url(images/lagos.jpg)">
+                        <div class="text">
+                            <h2 class="name" itemprop="name">
+                                <a onclick="popup" itemprop="url" href="Nigeria.php" title="Invest in Nigeria">Nigeria</a>
+                            </h2>
+                            <!-- <img title="Lagos" src="lagos.jpeg"> -->
+                        </div>
+                        <div class="action">
+                            <div class="main-score-rating">
+                                <div class="filling " style="width:100%">
+                                    <div itemprop="aggregateRating " itemscope itemtype="http://schema.org/aggregateRating ">
+                                        <meta itemprop="name " content="Rating " />
+                                        <!-- <meta itemprop="worstRating " value="0 " /><span itemprop="ratingValue ">5</span> <span itemprop="bestRating ">5</span><br /><span class="reviewCount "><span itemprop="reviewCount ">209</span> reviews</span> -->
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div> 
-                    <div class="attributes">
-                    </div>
-                </li>
+                        <div class="attributes">
+                        </div>
+                    </li>
 
-                <li class="Ghana" style="background: url(images/accra.jpeg)">
-                    <div class="text">
-                        <h2 class="name" itemprop="name">
-                            <a itemprop="url" href="Ghana.php" title="Invest in Ghana">Ghana</a>
-                        </h2>
-                        <!-- <img title="Accra" src="accra.jpg"> -->
-                    </div>
-                    <div class="action">
-                    </div>
-                    <div class="attributes"></div>
-                </li>
+                    <li class="Ghana" style="background: url(images/accra.jpeg)">
+                        <a href="Ghana.php">
+                            <div class="text">
+                                <h2 class="name" itemprop="name">
+                                    <a itemprop="url" href="Ghana.php" title="Invest in Ghana">Ghana</a>
+                                </h2>
+                                <!-- <img title="Accra" src="accra.jpg"> -->
+                            </div>
+                            <div class="action">
+                            </div>
+                            <div class="attributes"></div>
+                        </a>
+                    </li>
 
-                <li class="Rwanda" style="background: url(images/Kigali.jpg)">
-                    <div class="text">
-                        <h2 class="name" itemprop="name">
-                            <a itemprop="url" href="Rwanda.php" title="Invest in Rwanda">Rwanda</a>
-                        </h2>
-                        <!-- <img title="Kigali" src="kigali.jpg"> -->
-                    </div>
-                    <div class="action">
-                    </div>
-                    <div class="attributes"></div>
-                </li>
+                    <li class="Rwanda" style="background: url(images/Kigali.jpg)">
+                        <div class="text">
+                            <h2 class="name" itemprop="name">
+                                <a itemprop="url" href="Rwanda.php" title="Invest in Rwanda">Rwanda</a>
+                            </h2>
+                            <!-- <img title="Kigali" src="kigali.jpg"> -->
+                        </div>
+                        <div class="action">
+                        </div>
+                        <div class="attributes"></div>
+                    </li>
 
-                <li class="Angola" style="background: url(images/angola.jpg)">
-                    <div class="text">
-                        <h2 class="name" itemprop="name">
-                            <a itemprop="url" href="Angola.php" title="Invest in Angola">Angola</a>
-                        </h2>
-                    </div>
-                    <div class="action">
-                    </div>
-                    <div class="attributes"></div>
-                </li>
+                    <li class="Angola" style="background: url(images/angola.jpg)">
+                        <div class="text">
+                            <h2 class="name" itemprop="name">
+                                <a itemprop="url" href="Angola.php" title="Invest in Angola">Angola</a>
+                            </h2>
+                        </div>
+                        <div class="action">
+                        </div>
+                        <div class="attributes"></div>
+                    </li>
 
-                <li class="Kenya" style="background: url(images/nairobi.jpg)">
-                    <div class="text">
-                        <h2 class="name" itemprop="name">
-                            <a itemprop="url" href="Kenya.php" title="Invest in Kenya">Kenya</a>
-                        </h2>
-                        <!-- <img title="Nairobi" src="nairobi.jpg"> -->
-                    </div>
-                    <div class="action">
-                    </div>
-                    <div class="attributes"></div>
-                </li>
+                    <li class="Kenya" style="background: url(images/nairobi.jpg)">
+                        <div class="text">
+                            <h2 class="name" itemprop="name">
+                                <a itemprop="url" href="Kenya.php" title="Invest in Kenya">Kenya</a>
+                            </h2>
+                            <!-- <img title="Nairobi" src="nairobi.jpg"> -->
+                        </div>
+                        <div class="action">
+                        </div>
+                        <div class="attributes"></div>
+                    </li>
 
-                <li class="Egypt" style="background: url(images/cairo.jpg)">
-                    <div class="text">
-                        <h2 class="name" itemprop="name">
-                            <a itemprop="url" href="Egypt.php" title="Invest in Egypt">Egypt</a>
-                        </h2>
-                        <!-- <img title="Cairo" src="cairo.jpg"> -->
-                    </div>
-                    <div class="action">
-                    </div>
-                    <div class="attributes"></div>
-                </li>
+                    <li class="Egypt" style="background: url(images/cairo.jpg)">
+                        <div class="text">
+                            <h2 class="name" itemprop="name">
+                                <a itemprop="url" href="Egypt.php" title="Invest in Egypt">Egypt</a>
+                            </h2>
+                            <!-- <img title="Cairo" src="cairo.jpg"> -->
+                        </div>
+                        <div class="action">
+                        </div>
+                        <div class="attributes"></div>
+                    </li>
 
-                <li class="South Africa" style="background: url(images/joburg.jpg)">
-                    <div class="text">
-                        <h2 class="name" itemprop="name">
-                            <a itemprop="url" href="SouthAfrica.php" title="Invest in South Africa">South Africa</a>
-                        </h2>
-                        <!-- <img title="Johannesburg" src="joburg.jpg"> -->
-                    </div>
-                    <div class="action">
-                    </div>
-                    <div class="attributes"></div>
-                </li>
 
-                <li class="IvoryCoast" style="background: url(images/abidjan.jpg)">
-                    <div class="text">
-                        <h2 class="name" itemprop="name">
-                            <a itemprop="url" href="IvoryCoast.php" title="Invest in Ivory Coast">Ivory Coast</a>
-                        </h2>
-                        <!-- <img title="Abidjan" src="abidjan.jpg"> -->
-                    </div>
-                    <div class="action">
-                    </div>
-                    <div class="attributes"></div>
-                </li>
+                    <li class="South Africa" style="background: url(images/joburg.jpg)">
+                        <div class="text">
+                            <h2 class="name" itemprop="name">
+                                <a itemprop="url" href="SouthAfrica.php" title="Invest in South Africa">South Africa</a>
+                            </h2>
+                            <!-- <img title="Johannesburg" src="joburg.jpg"> -->
+                        </div>
+                        <div class="action">
+                        </div>
+                        <div class="attributes"></div>
+                    </li>
 
-                <!-- <li class="Casablanca">
+                    <li class="IvoryCoast" style="background: url(images/abidjan.jpg)">
+                        <div class="text">
+                            <h2 class="name" itemprop="name">
+                                <a itemprop="url" href="IvoryCoast.php" title="Invest in Ivory Coast">Ivory Coast</a>
+                            </h2>
+                            <!-- <img title="Abidjan" src="abidjan.jpg"> -->
+                        </div>
+                        <div class="action">
+                        </div>
+                        <div class="attributes"></div>
+                    </li>
+
+                    <!-- <li class="Casablanca">
                     <div class="text">
                         <h2 class="name" itemprop="name">
                             <a itemprop="url" href="cities/Casablanca.html" title="Invest in Casablanca, Morocco">Casablanca, Morocco</a>
@@ -155,17 +158,17 @@
                     </div>
                     <div class="attributes"></div>
                 </li> -->
+                </div>
             </div>
         </div>
-    </div>
 
 
-    <script src="" async defer></script>
+        <script src="" async defer></script>
 
-</body>
-<!-- <footer>
+    </body>
+    <!-- <footer>
     <p>Contact information: <a href="mailto:someone@example.com">
         someone@example.com</a>.</p>
 </footer> -->
 
-</html>
+    </html>
