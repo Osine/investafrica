3172507154102a530ec7d84357c6c8f720c93fc9
Osine Ikhianosime <osikhianosime@gmail.com>
2019-08-29 13:10:38 +0100

    proper placement of banner-text

--- web/header.php
+++ web/header.php
@@ -1,23 +1,24 @@
 <script src="javascript/myFunction.js"></script>
 <div class="topnav">
-<nav id="links">
-<a href="index.php"><img title="investafrica" src="images/investafrica.png" height="30px"></a>
+    <nav id="links">
+        <a href="index.php"><img title="investafrica" src="images/investafrica.png" height="30px"></a>
 
-    <!-- <input type="text" placeholder="Opportunity awaits..."> -->
-    <a class="active" href="index.php">Home</a>
-    <!-- <a href="#about">About</a>
+        <!-- <input type="text" placeholder="Opportunity awaits..."> -->
+        <a class="active" href="index.php">Home</a>
+        <!-- <a href="#about">About</a>
     <a class="login" href="#contact">Contact</a> -->
 
-</nav>
-<a href="javascript:void(0);" class="icon" onclick="myFunction()">
-<i class="fa fa-bars"></i>
-</a>
-</div>
-<div class ="hero-box">
-    <div class ="banner-text">
+    </nav>
+    <div class="banner-text">
         <p>Do Business across African Countries. Connect to African entrepreneurs and expand your business across Africa. </p>
-<div class="cta-box">
-    <button> INVEST NOW </button>
+    </div>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+    </a>
 </div>
-</div>
+<div class="hero-box">
+    <div class="cta-box">
+        <button> INVEST NOW </button>
+    </div>
 
+</div>
