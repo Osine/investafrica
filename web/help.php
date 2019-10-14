d2361192e817cbc5026250da40035fafb7392680
Osine Ikhianosime <osikhianosime@gmail.com>
2019-10-14 17:59:31 +0100

    help page

--- web/help.php
+++ web/help.php
@@ -15,6 +15,10 @@
 <?php include_once("header_city.php"); ?>
 
 <body>
+<!-- <button type="button" class="collapsible">Open Collapsible</button>
+<div class="content">
+  <p>Lorem ipsum...</p>
+</div> -->
     <h1>About the Metrics Used</h1>
 <h2>Institution</h2>
 <p>The Institutions pillar of the Global Competitiveness Index 4.0 assesses security, property rights, social capital, checks and balances, transparency and ethics, public-sector performance, future orientation of government, and corporate governance. See Appendix A of The Global Competitiveness Report 2019 for details.</p>
@@ -29,16 +33,32 @@
 <h2>Skills</h2>
 <p>The Skills pillar of the Global Competitiveness Index 4.0 assesses the general level of skills of the workforce and the quantity and quality of education. While the concept of educational quality is constantly evolving, important quality factors today include: developing digital literacy, interpersonal skills, and the ability to think critically and creatively. See Appendix A of The Global Competitiveness Report 2019 for details.</p>
 <h2>Product Market</h2>
-<p></p>
+<p>The Product market pillar of the Global Competitiveness Index 4.0 assesses the extent to which a country provides an even playing field for companies to participate in its markets. It is measured in terms of extent of market power, openness to foreign firms and the degree of market distortions. See Appendix A of The Global Competitiveness Report 2019 for details.</p>
 <h2>Labour Market</h2>
-<p></p>
+<p>The Labour market pillar of the Global Competitiveness Index 4.0 assesses the flexibility of the labor market, namely, the extent to which human resources can be reorganized and talent management”, namely, the extent to which human resources are leveraged. See Appendix A of The Global Competitiveness Report 2019 for details.</p>
 <h2>Financial System</h2>
-<p></p>
+<p>The Financial system pillar of the Global Competitiveness Index 4.0 assesses the depth, namely the availability of credit, equity, debt, insurance and other financial products, and the stability, namely, the mitigation of excessive risk-taking and opportunistic behavior of the financial system. See Appendix A of The Global Competitiveness Report 2019 for details.</p>
 <h2>Market Size</h2>
-<p></p>
+<p>The Market size pillar of the Global Competitiveness Index 4.0 assesses the size of the domestic and foreign markets to which a country’s firms have access. It is proxied by the sum of the value of consumption, investment and exports. See Appendix A of The Global Competitiveness Report 2019 for details.</p>
 <h2>Business Dynamism</h2>
-<p></p>
+<p>The Business dynamism pillar of the Global Competitiveness Index 4.0 assesses the private sector’s capacity to generate and adopt new technologies and new ways to organize work, through a culture that embraces change, risk, new business models, and administrative rules that allow firms to enter and exit the market easily. See Appendix A of The Global Competitiveness Report 2019 for details.</p>
 <h2>Innovation Capacity</h2>
-<p></p>
+<p>The Innovation capability pillar of the Global Competitiveness Index 4.0 assesses the quantity and quality of formal research and development; the extent to which a country’s environment encourages collaboration, connectivity, creativity, diversity and confrontation across different visions and angles; and the capacity to turn ideas into new goods and services. See Appendix A of The Global Competitiveness Report 2019 for details.</p>
+<script>
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+</script>
 </body>
 </html>
